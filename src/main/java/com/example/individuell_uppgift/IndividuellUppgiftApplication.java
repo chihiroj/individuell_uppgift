48d5a0f17713fbeb007b85d62697bb5ee7c2fbf8
chihiroj <77557825+chihiroj@users.noreply.github.com>
2025-02-01 13:53:36 +0100

    First commit.

---
+++ src/main/java/com/example/individuell_uppgift/IndividuellUppgiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.individuell_uppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividuellUppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividuellUppgiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/individuell_uppgift/IndividuellUppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.individuell_uppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividuellUppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
